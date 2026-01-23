a23854ff3ce872d7ba425599feca074076c3da75
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-01-23 05:57:13 -0500

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,19 @@
+"""
+Test suite for spiral-time memory framework.
+Covers operators, dynamics, CP-divisibility, and experimental protocols.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Spiral-Time Research Team"
+
+# Test modules
+__all__ = [
+    'test_operators',
+    'test_dynamics',
+    'test_cp_divisibility',
+    'test_state_independence',
+    'test_process_tensor_rank',
+    'test_non_markovianity',
+    'test_environmental_models',
+    'test_protocols'
+]
